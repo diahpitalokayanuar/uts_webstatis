9107a14dcb27a06c59ee15bd6dc3c92b3edaeaa8
Diah <diahpitalokayanuar@gmail.com>
2023-11-17 20:06:58 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function kirimData(){
+    alert('Data berhasil dikirim');
+    document.getElementById("regist").reset();
+}
+
+function kirimPesan(){
+    alert('Pesan berhasil dikirim');
+    document.getElementById("pesan").reset();
+}
